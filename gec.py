fb7a9210a6519d73e7c6e645b311f096bdd67959
jilquero <szczygielski.mateusz@wp.pl>
2022-01-04 00:48:35 +0100

    Add print and load preset data

---
+++ gec.py
@@ -0,0 +1,71 @@
+def print_matrix(matrix, solutions_order=None):
+    size = len(matrix)
+    if solutions_order is None:
+        solutions_order = [i for i in range(size)]
+
+    for row in range(size):
+        for col in solutions_order:
+            print(matrix[row][col], end="\t")
+        print(matrix[row][size])
+
+
+def print_equations(matrix, solutions_order=None):
+    size = len(matrix)
+    if solutions_order is None:
+        solutions_order = [i for i in range(size)]
+
+    for row in range(size):
+        for col in range(size):
+            print(str(matrix[row][col]) + "x" + str(solutions_order[col] + 1) + " ", end="")
+            if matrix[row][col + 1] >= 0 and col < size - 1:
+                print("+", end="")
+        print("=", matrix[row][size])
+
+
+def load_preset_data():
+    matrix = [[[1, 2, -1, 2, 0],
+               [1, -2, 4, 4, 4],
+               [0, -3, 1.5, 7, 0],
+               [0, -1, 1, 6, -1]], [[14, -13, 3, -16, -42, -37],
+                                    [3.5, -18, 13, -23.75, -21, -5.5],
+                                    [3.5, 3, -5.25, 9.25, 10.5, 12.5],
+                                    [2, 14.5, -10.5, 18.5, 21, 23.5],
+                                    [1.5, 6.75, -9.25, 17, 10.5, -45.25]]]
+
+    print("Select equations to solve: ")
+    print("\n[1]")
+    print_equations(matrix[0])
+    print("\n[2]")
+    print_equations(matrix[1])
+
+    selected_matrix = int(input("Option: "))
+
+    if selected_matrix == 1:
+        return matrix[0]
+    elif selected_matrix == 2:
+        return matrix[1]
+
+
+def load_own_data():
+    pass
+
+
+def load_data():
+    print("Select data source")
+    print("[1] Preset data")
+    print("[2] Own data")
+
+    selected_input_method = int(input("Option: "))
+
+    if selected_input_method == 1:
+        return load_preset_data()
+    elif selected_input_method == 2:
+        return load_own_data()
+
+
+def main():
+    matrix = load_data()
+
+
+if __name__ == "__main__":
+    main()
